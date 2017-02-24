0ee3b114b08c3159d3491e2c1f5ccfea5e45ac07
Alexander Miehe <alexander.miehe@tourstream.eu>
2017-02-24 09:06:07 +0100

    * add copyright stuff

--- Tests/RedisLockStrategyTest.php
+++ Tests/RedisLockStrategyTest.php
@@ -14,7 +14,7 @@ namespace Tourstream\RedisLockStrategy\Tests;
  *  This script is part of the Typo3 project. The Typo3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  The GNU General Public License can be found at
